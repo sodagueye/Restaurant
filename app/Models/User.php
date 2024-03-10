073d23f0437abc124c8267d11e1cf28e20be6de5
DELL LATITUDE 7470 <gueyesoda56@gmail.com>
2024-03-10 17:27:31 +0000

    Authentification

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,21 +6,35 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Tymon\JWTAuth\Contracts\JWTSubject;
+
+
+
 use Laravel\Sanctum\HasApiTokens;
 
-class User extends Authenticatable
+class User extends Authenticatable implements JWTSubject
 {
     use HasApiTokens, HasFactory, Notifiable;
 
     /**
      * The attributes that are mass assignable.
      *
+     * 
      * @var array<int, string>
      */
     protected $fillable = [
-        'name',
+        'nom',
+        'prenom',
+        'telephone',
+        'image',
+        'adresse',
+        'statut_compte',
+        'role',
         'email',
-        'password',
+        'email_verified_at',
+        'password'
+
+
     ];
 
     /**
@@ -42,4 +56,33 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
-}
+
+
+  
+    
+        // Rest of your User model code
+    
+        /**
+         * Get the identifier that will be stored in the subject claim of the JWT.
+         *
+         * @return mixed
+         */
+        public function getJWTIdentifier()
+        {
+            return $this->getKey();
+        }
+    
+        /**
+         * Return a key value array, containing any custom claims to be added to the JWT.
+         *
+         * @return array
+         */
+        public function getJWTCustomClaims()
+        {
+            return [];
+        }
+    }
+    
+
+
+
--- config/app.php
+++ config/app.php
@@ -156,6 +156,7 @@ return [
     */
 
     'providers' => ServiceProvider::defaultProviders()->merge([
+
         /*
          * Package Service Providers...
          */
@@ -167,6 +168,7 @@ return [
         App\Providers\AuthServiceProvider::class,
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
+        Tymon\JWTAuth\Providers\LaravelServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
     ])->toArray(),
 
